a54d284c634842489e6d84d46de522fb5688a21f
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-05-13 10:08:51 +0300

    Init commit

---
+++ src/main/java/com/example/e3stpavel/knorders/KnOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.example.e3stpavel.knorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/e3stpavel/knorders/KnOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e3stpavel.knorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
